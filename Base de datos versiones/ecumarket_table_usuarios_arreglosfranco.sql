724b6257c1ebb0212e62f9f7fb8a750d0cce93ad
xGeo <Geo7u7r@gmail.com>
2025-04-17 14:22:22 -0500

    ecumarket_table_usuarios_arreglosfranco.sql

--- Base de datos versiones/ecumarket_table_usuarios_arreglosfranco.sql
+++ Base de datos versiones/ecumarket_table_usuarios_arreglosfranco.sql
@@ -6,17 +6,18 @@
 --
 
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `correo` varchar(30) NOT NULL,
   `contraseña` varchar(25) NOT NULL,
   `nombrecompleto` varchar(100) NOT NULL,
   `telefono` int(11) NOT NULL,
-  `ciudad` varchar(100) NOT NULL
+  `ciudad` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `correo`, `contraseña`, `nombrecompleto`,`telefono`, `ciudad`) VALUES
-(1,  'juancito@hotmail.com','1234' ,'Juan Pérez','123456', 'Quito');
+INSERT INTO `usuarios` (`id`, `correo`, `contraseña`, `nombrecompleto`, `telefono`, `ciudad`) VALUES
+(1,  'juancito@hotmail.com', '1234', 'Juan Pérez', '123456', 'Quito');
